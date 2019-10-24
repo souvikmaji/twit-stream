64970b72ce3193e81ad160e88d50e22e484027f1
souvikmaji <souvikmaji94@gmail.com>
2019-10-24 08:29:06 +0530

    movc structure

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var router = express.Router();
-
-// Home page route
-router.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-module.exports = router;
