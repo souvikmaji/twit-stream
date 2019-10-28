0c7dfbc344219e23c3bd36e583044b4ec0f5082c
souvikmaji <souvikmaji94@gmail.com>
2019-10-28 12:28:49 +0530

    added more info in a tweet card

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,34 @@
-$(document).ready(function () {	
+$(document).ready(function () {
 
 	const newCard = function (tweet) {
-		return	`<div class="card card-md w-75 mx-auto">
-			<div class="card-body">
-				<div class="card-header">
-					<h5>${tweet.user.screen_name}</h5>
-				</div>
-				<div class="card-body">
-					<div class="card-text">${tweet.text}</div>
-				</div>
-
-			</div>
-		</div>
-			<br />`;
+		return	`<div class="card card-md w-75 mx-auto mb-2">
+        <div class="card-header bg-transparent">
+          <h5 class="card-title">${tweet.user.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">@${tweet.user.screen_name}</h6>
+        </div>
+        <div class="card-body">
+          <div class="card-text">${tweet.text}</div>
+        </div>
+        <div class="card-footer text-muted bg-transparent">
+          <span><i class="far fa-heart"></i>${tweet.favorite_count}</span>
+          <span><i class="fas fa-retweet"></i>${tweet.retweet_count}</span>
+          <span>${tweet.created_at} </span>
+        </div>
+      </div>
+       <br />`;
 	};
 
 	var socket = io();
 
-	$("#source-form").submit(function (event) {	
+	$("#source-form").submit(function (event) {
 		event.preventDefault();
 		$("#feed").empty();
 		socket.emit("subscribe", $("#source").val());
 		return false;
-	}); 
+	});
 
 	socket.on("tweet", function (tweet) {
-		console.log(tweet);	
+		console.log(tweet);
 		let cardContent =  newCard(tweet);
 
 		$("#feed").prepend(cardContent);
--- routes/tweets.js
+++ routes/tweets.js
@@ -3,27 +3,21 @@ module.exports = function (io, twit) {
 	var router = app.Router();
 
 	io.on("connection", function (socket) {
-		console.log("A user connected");
-		
+
 		socket.on("subscribe", function (source) {
 			console.log("message: " + source);
 			let stream = twit.stream("statuses/filter", { track: [source] });
-			console.log("came here");
+
 			stream.on("tweet", function (tweet) {
 				io.emit("tweet",tweet);
-			});	
+			});
+			
 			socket.on("disconnect", function () {
 				stream.stop();
 			});
 		});
 
-		
-
 	});
 
 	return router;
-};
-
-const sendTweets = function (source, twit, res) {
-	
 };
