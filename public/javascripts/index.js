83cf407cfbab7c617a93e76e0a35d2a51d7fc8e4
souvikmaji <souvikmaji94@gmail.com>
2019-10-29 21:04:52 +0530

    stylized twitter cards

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,20 +4,20 @@ $(document).ready(function () {
 
 	const newCard = function (tweet) {
 		return	`<div class="card card-md w-75 mx-auto mb-2">
-        <div class="card-header bg-transparent">
-          <h5 class="card-title">${tweet.user.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">@${tweet.user.screen_name}</h6>
+          <div class="card-header bg-transparent">
+            <h5 class="card-title text-primary">${tweet.user.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">@${tweet.user.screen_name}</h6>
+          </div>
+          <div class="card-body">
+            <div class="card-text">${tweet.text}</div>
+          </div>
+          <div class="card-footer text-muted bg-transparent">
+            <span class="tab-space text-danger"><i class="fas fa-heart"></i>${tweet.favorite_count}</span>
+            <span class="tab-space text-success"><i class="fas fa-retweet"></i>${tweet.retweet_count}</span>
+            <span class="tab-space text-info">${tweet.created_at}</span>
+          </div>
         </div>
-        <div class="card-body">
-          <div class="card-text">${tweet.text}</div>
-        </div>
-        <div class="card-footer text-muted bg-transparent">
-          <span class="tab-space"><i class="far fa-heart"></i>${tweet.favorite_count}</span>
-          <span class="tab-space"><i class="fas fa-retweet"></i>${tweet.retweet_count}</span>
-          <span class="tab-space">${tweet.created_at}</span>
-        </div>
-      </div>
-       <br />`;
+         <br />`;
 	};
 
 	let socket = io();
