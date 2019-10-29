6455757c74d74d8355cf1345916b2e979688d734
souvikmaji <souvikmaji94@gmail.com>
2019-10-29 21:59:53 +0530

    view on twitter button added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 		return	`<div class="card card-md w-75 mx-auto mb-2">
           <div class="card-header bg-transparent">
 						<img src="${tweet.user.profile_image_url_https}" class="img-responsive rounded-circle float-left mr-2">
+						<a href="https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}" target="_blank" title="Show on Twitter"><i class="fab fa-twitter float-right"></i></a>
             <h5 class="card-title text-primary">${tweet.user.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">@${tweet.user.screen_name}</h6>
           </div>
